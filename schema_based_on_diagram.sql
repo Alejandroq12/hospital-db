760801064e970a1b3a879fb9e3fd52d72995b093
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-13 15:01:55 -0500

    create invoices_items table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -23,3 +23,15 @@ CREATE TABLE invoices (
   PRIMARY KEY(id),
   FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
+
+CREATE TABLE invoice_items (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT,
+  treatment_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
